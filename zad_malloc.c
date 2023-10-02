67a769fa58af042d0da010599baed67cf0832274
DRulin <daniellw1999@gmail.com>
2023-10-02 10:44:57 +0200

    Poprawiona zad_malloc.c

--- zad_malloc.c
+++ zad_malloc.c
@@ -7,7 +7,6 @@ int main() {
     printf("Podaj liczba slow do wprowadzenia: ");
     scanf("%d", &liczba_slow);
     wsk = (char **)malloc(liczba_slow*sizeof(char *));
-    printf("%p %p\n\n", wsk, &wsk);
     if (wsk == NULL) {
         fprintf(stderr, "Nie udalo sie przydzielic pamieci 1.\n");
         exit(EXIT_FAILURE);
@@ -16,16 +15,14 @@ int main() {
     for(i = 0; i < liczba_slow; i++) {
         scanf("%s", temp_ch);
         wsk[i] = (char *)malloc(sizeof(temp_ch)*sizeof(char));
-        printf("%p | %p\n", wsk[i], &wsk[i]);
         if (wsk[i] == NULL) {
             fprintf(stderr, "Nie udalo sie przydzielic pamieci 2.\n");
             exit(EXIT_FAILURE);
         }
         strcpy(wsk[i], temp_ch);
     }
-    printf("Udalo sie!\n");
     for(i = 0; i < liczba_slow; i++) {
-        printf("%s %p %p\n", wsk[i], wsk[i], &wsk[i]);
+        printf("%s\n", wsk[i]);
         free(wsk[i]);
     }
     return 0;
