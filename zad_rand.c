0c807b92c1b71f4d6481acdb694f5c72296fa0e2
DRulin <daniellw1999@gmail.com>
2023-10-02 10:51:49 +0200

    Dodano zad_rand.c

---
+++ zad_rand.c
@@ -0,0 +1,11 @@
+#include"mheader.h"
+#include<time.h>
+
+int main() {
+    int i;
+    srand(time(NULL));
+    for (i = 0; i < 20; i++) {
+        printf("%d\n", rand()%5);
+    }
+    return 0;
+}
